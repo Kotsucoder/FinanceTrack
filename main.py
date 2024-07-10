186fe16fd986658fee1adc30c67a89c2a5724373
Marcus Kotsu <whoami@kotsu.red>
2024-07-10 15:54:17 -0700

    Improved documentation format.

--- main.py
+++ main.py
@@ -18,14 +18,15 @@ saveLoc = 'data/'
 # help command
 # init save-location ***
 
-#----------------------------------------------------------------------------------------
-# This portion collects the current save location, commands, and attributes.
-
 def setSave(location = 'data/'):
     """
-    User command to set the current save location.\n
-    Will set to the default 'data/' if location attribute is not set.\n
-    This function sets Global Variable saveLoc.
+    Sets the current save location. Creates directory if it does not yet exist.
+
+    Args:
+        location (str: 'data/'): Path to the desired save location. Defaults to 'data/' if not set.
+
+    Returns:
+        None
     """
     global saveLoc
     if location[-1] != '/':
@@ -37,44 +38,29 @@ def setSave(location = 'data/'):
     if not os.path.exists(location):
         os.makedirs(location)
 
-# Determines the current save location, sets to default if saveloc.bills doesn't exist.
-try:
-    dataSource = open('saveloc.bills', 'r')
-    saveLoc = dataSource.read()
-    dataSource.close()
-except:
-    setSave()
 
-# Grabs the current command.
-try:
-    command = argv[1]
-except:
-    command = None
-
-# Grabs any command attribute.
-try:
-    print(argv[2])
-    attribute = argv[2:]
-except:
-    attribute = [None]
-
-#----------------------------------------------------------------------------------------
-
-# Opens file in the current save location.
 def openFile(filename, rw):
     """
-    Opens the given file in the current save location.\n
-    Requires the filename and rw attribute.\n
-    rw attribute supports all the same modes as open(), but must be explicitly defined.\n
-    Global Variable saveLoc must be set.
+    Opens the given file at the current save location.
+
+    Requirement:
+        Global variable saveLoc must be set.
+
+    Args:
+        filename (str): Name of the file to open.
+        rw (str): Mode to open the file. Supports all modes supported by open() function.
+
+    Returns:
+        I/O Datastream for opened file.
     """
     return open(f'{saveLoc}{filename}', rw)
 
 def checkInitFile():
     """
-    This function checks if the init file exists in the current save location.\n
-    Returns true if the file exists, returns false if the file does not exist.\n
-    Global Variable saveLoc must be set.
+    Checks for the init.bills file in current location.
+
+    Returns:
+        bool: True if file exists, False if file does not exist.
     """
     try:
         initFile = openFile('init.bills', 'r')
@@ -84,38 +70,53 @@ def checkInitFile():
 
 def initProcess():
     """
-    This function prepares the main files that FinanceTrack operates with.\n
-    This includes:\n
-    categories.bills - Contains categories that can be used for income and expenses.\n
-    income.bills - Contains all income data. Initialized with headers.\n
-    expense.bills - Contains all epense data. Initialized with headers.\n
-    \n
-    This function does not create budget.bills.
+    Prepares the main files that FinanceTrack operates with.
+
+    Files Created:
+        categories.bills (txt): Contains categories that can be used for income and expenses.  
+        income.bills (csv): Contains all income data. Initialized with headers.  
+        expense.bills (csv): Contains all expense data. Initialized with headers.  
+
+    Returns:
+        bool: True if successful, False if process failed.
     """
-    categories = ['Bills\n', 'Auto\n', 'Groceries\n', 'Medical\n', 'Loans\n', 'Subscriptions\n']
-    catFile = openFile('categories.bills', 'w')
-    catFile.writelines(categories)
-    catFile.close()
+    try:
+        categories = ['Bills\n', 'Auto\n', 'Groceries\n', 'Medical\n', 'Loans\n', 'Subscriptions\n']
+        catFile = openFile('categories.bills', 'w')
+        catFile.writelines(categories)
+        catFile.close()
+    except:
+        return False
 
-    incomeFile = openFile('income.bills', 'w')
-    incomeWriter = csv.writer(incomeFile)
-    incomeWriter.writerow(["date", "category", "amount", "description"])
-    incomeFile.close()
+    try:
+        incomeFile = openFile('income.bills', 'w')
+        incomeWriter = csv.writer(incomeFile)
+        incomeWriter.writerow(["date", "category", "amount", "description"])
+        incomeFile.close()
+    except:
+        return False
 
-    expenseFile = openFile('expense.bills', 'w')
-    expenseWriter = csv.writer(expenseFile)
-    expenseWriter.writerow(["date", "category", "amount", "description"])
-    expenseFile.close()
+    try:
+        expenseFile = openFile('expense.bills', 'w')
+        expenseWriter = csv.writer(expenseFile)
+        expenseWriter.writerow(["date", "category", "amount", "description"])
+        expenseFile.close()
+    except:
+        return False
+
+    return True
 
 def init(save_location = None):
     """
-    This is the user-ran init function.\n
-    save_location is an optional attribute. It does nothing if not set.\n
-    setSave() will be ran if save_location is set.\n
-    This function will run checkInitFile() to see if the program has previously been initialized.
-    If the init file exists, user will be asked if they want to proceed with initialization, this will erase all existing data.\n
-    Initialization is performed by initProcess().
+    Initializes the program in the current save location. Also sets save location if specified.
+
+    Args:
+        save_location (str): Path to the desired save location. Optional.
+
+    Returns:
+        bool: True if successful, False if process failed.
     """
+    initSuccess = False
     if save_location:
         setSave(save_location)
 
@@ -124,28 +125,55 @@ def init(save_location = None):
     if openSuccess:
         question = input("This program has already been initialized. Are you sure you'd like to continue? All existing data will be lost.\nType 'Yes' to confirm: ")
         if question == 'Yes':
-            initProcess()
+            initSuccess = initProcess()
+        else:
+            initSuccess = True
     else:
-        initProcess()
+        initSuccess = initProcess()
         initFile = openFile('init.bills', 'w')
+        initFile.close()
+
+    return initSuccess
 
 def add_income(category, amount, description):
     pass
 
+if __name__ == '__main__':
+    # Determines the current save location, sets to default if saveloc.bills doesn't exist.
+    try:
+        dataSource = open('saveloc.bills', 'r')
+        saveLoc = dataSource.read()
+        dataSource.close()
+    except:
+        setSave()
 
-match command:
-    case 'init':
-        init(attribute[0])
-    case 'set-save':
-        if attribute[0] == None:
-            print("Warning: Save location will be set to the default. Is this what you want to do? (Y/N)")
-            setDefault = input("> ")
-            if setDefault == "Y":
-                attribute[0] = 'data/'
-            else:
-                attribute[0] = saveLoc
-        setSave(attribute[0])
-    case None:
-        print("Please enter an argument in the command line.")
-    case _:
-        print("Invalid command. Use 'list commands' to see list of commands. Use 'help' to learn more about a command.")
+    # Grabs the current command.
+    try:
+        command = argv[1]
+    except:
+        command = None
+
+    # Grabs any command attribute.
+    try:
+        testArgv = argv[2]
+        attribute = argv[2:]
+    except:
+        attribute = [None]
+
+    # Runs the command
+    match command:
+        case 'init':
+            init(attribute[0])
+        case 'set-save':
+            if attribute[0] == None:
+                print("Warning: Save location will be set to the default. Is this what you want to do? (Y/N)")
+                setDefault = input("> ")
+                if setDefault == "Y":
+                    attribute[0] = 'data/'
+                else:
+                    attribute[0] = saveLoc
+            setSave(attribute[0])
+        case None:
+            print("Please enter an argument in the command line.")
+        case _:
+            print("Invalid command. Use 'list commands' to see list of commands. Use 'help' to learn more about a command.")
